15d69942b8eec79c65f78cc5b3a8ffd529435c1d
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2024-12-10 01:16:42 -0500

    Agregando modelo a la clase user con nombres, apellidos, correo, fecha_nacimiento, numero_celular y sexo

--- CineBackend/users/models.py
+++ CineBackend/users/models.py
@@ -10,12 +10,12 @@ class User(models.Model):
         ('O', 'Otro'),
     ]
 
-    nombres = models.CharField(max_length=100)  # Nombre(s) del usuario
-    apellidos = models.CharField(max_length=100)  # Apellido(s) del usuario
-    correo = models.EmailField(unique=True)  # Correo electrónico único
-    fecha_nacimiento = models.DateField()  # Fecha de nacimiento
-    numero_celular = models.CharField(max_length=15, unique=True)  # Número de celular único
-    sexo = models.CharField(max_length=1, choices=SEX_CHOICES)  # Sexo con opciones predefinidas
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    fecha_nacimiento = models.DateField()
+    numero_celular = models.CharField(max_length=15, unique=True)
+    sexo = models.CharField(max_length=1, choices=SEX_CHOICES)
 
     def __str__(self):
         return f"{self.nombres} {self.apellidos} - {self.correo}"
