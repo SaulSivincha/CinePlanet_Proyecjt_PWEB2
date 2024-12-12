48d66cedf43b1fa850f5ac3207d02b1f4e5de8e6
SS@SaulSivincha <139631778+SaulSivincha@users.noreply.github.com>
2024-12-12 13:51:33 -0500

    Update NavBar.jsx

---
+++ CinePlanet-FrontEnd/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import './NavBar.css';
+ 
+function NavBar() {
+   return (
+     <nav>
+       <ul>
+         <li><a href="/">cinePlanet</a></li>
+         <li><a href="/peliculas">Peliculas</a></li>
+				 <li><a href="/cines">Cines</a></li>
+         <li><a href="/promociones">Promociones</a></li>
+         <li><a href="/dulceria">Dulceria</a></li>
+         <li><a href="/carrito">Carrito de Compras</a></li>
+       </ul>
+     </nav>
+	);
+}
+export default NavBar;
