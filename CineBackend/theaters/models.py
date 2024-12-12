22f3645055a2af2740acde21374549de60ca2922
AntonyCondori <acondoricai@unsa.edu.pe>
2024-12-11 22:57:54 -0500

    Agregando modelos de la app theaters

--- CineBackend/theaters/models.py
+++ CineBackend/theaters/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Theater(models.Model):
+    FUNCTION_TYPE_CHOICES = [
+        ('2D', '2D'),
+        ('3D', '3D'),
+        ('REGULAR', 'Regular'),
+    ]
+
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    function_types = models.ManyToManyField(
+        'FunctionType',
+        help_text="Select function types available in this theater"
+    )
+
+    def __str__(self):
+        return self.name
+
+class FunctionType(models.Model):
+    name = models.CharField(max_length=20, choices=Theater.FUNCTION_TYPE_CHOICES, unique=True)
+
+    def __str__(self):
+        return self.name
