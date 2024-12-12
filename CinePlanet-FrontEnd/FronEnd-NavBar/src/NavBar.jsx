182a22f6e2b6d86d192f1622652960fa2daa9337
SaulSivincha <ssivincham@unsa.edu.pe>
2024-12-11 20:25:36 -0500

    First steps with component navbar

---
+++ CinePlanet-FrontEnd/FronEnd-NavBar/src/NavBar.jsx
@@ -0,0 +1,18 @@
+import './NavBar.css'
+
+function NavBar(){
+  return(
+    <>
+      <ul>
+        <li>cinePlanet</li>
+        <li>Peliculas</li>
+        <li>Cines</li>
+        <li>Promociones</li>
+        <li>Dulceria</li>
+        <li>Carrito de Compras</li>
+      </ul>
+    </>
+  )
+}
+
+export default NavBar
