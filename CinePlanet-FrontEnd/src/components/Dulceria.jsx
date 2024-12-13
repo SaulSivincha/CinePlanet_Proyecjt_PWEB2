e4b3cbdb4c8e3324918adef1c8a77a08a6b8af44
DieguitoBot <diegorym10@gmail.com>
2024-12-13 13:03:04 -0500

    Añadiendo el componente con el css - Dulceria

--- CinePlanet-FrontEnd/src/components/Dulceria.jsx
+++ CinePlanet-FrontEnd/src/components/Dulceria.jsx
@@ -1,10 +1,12 @@
 import NavBar from './NavBar.jsx';
 import '../styles/Dulceria.css';
+import DulceriaP from './DulceriaP.jsx';
 
 function Dulceria() {
     return (
          <div>
             <NavBar />
+            <DulceriaP />
         </div>
     )
 }
