280a0183a114a870d9ae486aa6d4fca87afc6de0
DaniWik <bsenciaa@unsa.edu.pe>
2024-12-13 00:25:30 -0500

    primeros componentes del apartado peliculas

--- CinePlanet-FrontEnd/src/components/Peliculas.jsx
+++ CinePlanet-FrontEnd/src/components/Peliculas.jsx
@@ -1,9 +1,11 @@
 import NavBar from './NavBar.jsx';
 import '../styles/Peliculas.css';
+import Funsion from './Funsion.jsx';
 function Peliculas() {
     return(
         <div>
              <NavBar />
+             <Funsion/>
         </div>
     );
 }
