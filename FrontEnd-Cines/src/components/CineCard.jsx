527d0dab5dc50f841d398db611c4344933ffa7d7
roydanpe <rapazaan@unsa.edu.pe>
2024-12-13 19:13:09 -0500

    Editando el css para que se vea mejor

--- FrontEnd-Cines/src/components/CineCard.jsx
+++ FrontEnd-Cines/src/components/CineCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './CineCard.css';
 
-function CineCard({ nombre, direccion, imagen }) {
+function CineCard({ nombre, direccion, imagen, formatos }) {
   return (
     <div className="cine-card">
       <img src={imagen} alt={nombre} className="cine-card-img" />
       <h2>{nombre}</h2>
       <p>{direccion}</p>
+      <p className="formatos"> {formatos.join(', ')}</p>
     </div>
   );
 }
