3d68ed097407fc3f53ab0cb8ac25284e75faae7a
SaulSivincha <ssivincham@unsa.edu.pe>
2024-12-15 18:34:33 -0500

    Changes added to branch Frontend

--- CinePlanet-FrontEnd/src/components/Promociones.jsx
+++ CinePlanet-FrontEnd/src/components/Promociones.jsx
@@ -1,11 +1,13 @@
 import NavBar from './NavBar.jsx';
+import PromocionBar from './PromocionBar.jsx'
 import '../styles/Promociones.css';
 function Promociones() {
     return (
         <div>
             <NavBar />
+            <PromocionBar />
         </div>
     )
 }
 
-export default Promociones;
+export default Promociones;;
