8a05261e4086616ec664200b5a9f970800cb7a66
SaulSivincha <ssivincham@unsa.edu.pe>
2024-12-15 21:04:22 -0500

    changes added

--- CinePlanet-FrontEnd/src/components/Peliculas.jsx
+++ CinePlanet-FrontEnd/src/components/Peliculas.jsx
@@ -1,6 +1,7 @@
 import NavBar from './NavBar.jsx';
 import '../styles/Peliculas.css';
 import Funsion from './Funsion.jsx';
+import  Device  from '../devices/Device.jsx';
 function Peliculas() {
     return(
         <div>
