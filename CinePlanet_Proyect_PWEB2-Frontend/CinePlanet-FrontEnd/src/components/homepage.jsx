df743d5ed16cdaa394ff3a3e0e78559878921758
DieguitoBot <diegorym10@gmail.com>
2024-12-16 18:23:54 -0500

    Añadiendo ultimo detalles para Footer y HomePage

--- CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/homepage.jsx
+++ CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/homepage.jsx
@@ -3,11 +3,13 @@ import NavBar from './NavBar.jsx';
 import Home from './Home.jsx';
 import FooterP1 from './FooterP1.jsx';
 import FooterP2 from './FooterP2.jsx';
+import Carousel from './Carousel.jsx';
 
 function Homepage() {
     return (
       <div >
             <NavBar />
+            <Carousel />
             <Home/>
             <FooterP1 />
             <FooterP2 />
