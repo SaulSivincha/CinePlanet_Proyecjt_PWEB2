d24bf31303bc2af5a0abb14d7ac9a7046a1e0fc1
DieguitoBot <diegorym10@gmail.com>
2024-12-17 13:59:08 -0500

    Rama de admin

--- BackEnd/CineBackend/promociones/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ListaPromociones, VistaPromocion
-
-urlpatterns = [
-    path('', ListaPromociones.as_view(), name='lista-promociones'),
-    path('<int:id>/', VistaPromocion.as_view(), name='vista-promocion'),
-]
--- BackEnd/CineBackend/usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ListaUsuarios, VistaUsuario, RegistroUsuario, CustomTokenView
-
-urlpatterns = [
-    path('', ListaUsuarios.as_view(), name='lista-usuarios'),
-    path('<int:id>/', VistaUsuario.as_view(), name='vista-usuario'),
-    path('register/', RegistroUsuario, name='registro-usuario'),
-    path('token/', CustomTokenView.as_view(), name='token_obtain'),
-]
