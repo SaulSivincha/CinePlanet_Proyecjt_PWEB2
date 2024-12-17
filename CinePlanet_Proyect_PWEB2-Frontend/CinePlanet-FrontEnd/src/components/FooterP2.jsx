079ff5842812ce5320b9527cb91fff9864482d73
DieguitoBot <diegorym10@gmail.com>
2024-12-16 23:31:27 -0500

    Arreglando css con porcentajes-Solo los dos footer y carousel

--- CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/FooterP2.jsx
+++ CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/FooterP2.jsx
@@ -8,25 +8,25 @@ function FooterP2() {
                   <div>
                       <span className='footer-span'>Síguenos en: </span>
                       <a href="https://www.facebook.com" className="text-light me-2">
-                          <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kccZ7RZgXm69K1Z8YyLeiSusF46845q9dvB6F95rrucZSY6U" alt="Facebook" style={{ width: "50px", height: "50px" }} />
+                          <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ3kccZ7RZgXm69K1Z8YyLeiSusF46845q9dvB6F95rrucZSY6U" alt="Facebook" style={{ width: "40px", height: "40px" }} />
                       </a>
                       <a href="https://twitter.com" className="text-light me-2">
-                          <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQBZfECrZYS6R8QExFwKDYSzqWUBPdygLg4JIjCFxwFQAZd2z3R" alt="Twitter" style={{ width: "50px", height: "50px" }} />
+                          <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQBZfECrZYS6R8QExFwKDYSzqWUBPdygLg4JIjCFxwFQAZd2z3R" alt="Twitter" style={{ width: "40px", height: "40px" }} />
                       </a>
                       <a href="https://www.youtube.com" className="text-light me-2">
-                          <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS3VMcPg9981V_6becJz44xxWwEysykddM_w80yg5PaIttQ3ckP" alt="YouTube" style={{ width: "50px", height: "50px" }} />
+                          <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS3VMcPg9981V_6becJz44xxWwEysykddM_w80yg5PaIttQ3ckP" alt="YouTube" style={{ width: "40px", height: "40px" }} />
                       </a>
                       <a href="https://www.instagram.com" className="text-light">
-                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjXjZ-E4L7LhLsTL3lfksqQ75V15UVxTcBp9qco5Ap7HzR0ykx" alt="Instagram" style={{ width: "50px", height: "50px" }} />
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjXjZ-E4L7LhLsTL3lfksqQ75V15UVxTcBp9qco5Ap7HzR0ykx" alt="Instagram" style={{ width: "40px", height: "40px" }} />
                       </a>
                   </div>
                   <div>
                       <span className='footer-span'>Descarga la app: </span>
                       <a href="#" className="text-light me-2">
-                          <img src="https://png.pngtree.com/element_our/md/20180301/md_5a98274408dd7.jpg" alt="PlayStore" style={{ width: "60px", height: "60px" }} />
+                          <img src="https://png.pngtree.com/element_our/md/20180301/md_5a98274408dd7.jpg" alt="PlayStore" style={{ width: "40px", height: "40px" }} />
                       </a>
                       <a href="#" className="text-light">
-                          <img src="https://w7.pngwing.com/pngs/566/77/png-transparent-apple-logo-apple-watch-logo-apple-logo-heart-logo-computer-wallpaper-thumbnail.png" alt="Apple" style={{ width: "50px", height: "50px" }} />
+                          <img src="https://w7.pngwing.com/pngs/566/77/png-transparent-apple-logo-apple-watch-logo-apple-logo-heart-logo-computer-wallpaper-thumbnail.png" alt="Apple" style={{ width: "40px", height: "40px" }} />
                       </a>
                   </div>
                   <a href="/libro-reclamaciones" className="">
