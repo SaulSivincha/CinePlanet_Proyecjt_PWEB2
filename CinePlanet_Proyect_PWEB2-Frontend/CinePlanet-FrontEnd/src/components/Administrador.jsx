1805d9a2924a1983419b9dcad592f6be9d6fc090
SaulSivincha <ssivincham@unsa.edu.pe>
2024-12-18 00:24:23 -0500

    Admin modiicado

--- CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/Administrador.jsx
+++ CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/Administrador.jsx
@@ -1,19 +1,36 @@
-import Register from './Register';
 import FormPromociones from './FormPromociones';
 import FormFunsion from './CompPeliculas/FormFunsion';
 import EliminarFunsion from './CompPeliculas/EliminarFunsion';
 import DulceriaForm from './DulceriaForm';
-
+import React, { useState } from 'react';
+import '../styles/Admi.css';
 function Administrador() {
-    return (
-        <div>
-            <Register />
-            <FormPromociones />
-            <FormFunsion/>
-            <EliminarFunsion/>
-            <DulceriaForm />
-        </div>
-    );
+  const [activeSection, setActiveSection] = useState('Cine');
+
+  const handleSectionClick = (section) => {
+    setActiveSection(section);
+  };
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li onClick={() => handleSectionClick('Cine')}>Form. Cines</li>
+          <li onClick={() => handleSectionClick('Peliculas')}>Form. Películas</li>
+          <li onClick={() => handleSectionClick('Promociones')}>Form. Promociones</li>
+          <li onClick={() => handleSectionClick('Dulceria')}>Form. Dulcería</li>
+        </ul>
+      </nav>
+      <div>
+        {activeSection === 'Cine' && <FormFunsion />}
+        {activeSection === 'Cine' && <EliminarFunsion />}
+        {activeSection === 'Peliculas' && <FormFunsion />}
+        {activeSection === 'Peliculas' && <EliminarFunsion />}
+        {activeSection === 'Promociones' && <FormPromociones />}
+        {activeSection === 'Dulceria' && <DulceriaForm />}
+      </div>
+    </div>
+  );
 }
 
 export default Administrador;
