e5dad9c79e6470a3f09ff715dc1edfa82ab82674
DaniWik <bsenciaa@unsa.edu.pe>
2024-12-17 23:19:29 -0500

    Agregando datos a Admi.jsx

--- CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/Admi.jsx
+++ CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/Admi.jsx
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import '../styles/Admi.css';
 import FormPromociones from './FormPromociones';
 import CineForm from './CineForm';
+import FormFunsion from './CompPeliculas/FormFunsion';
+import EliminarFunsion from './CompPeliculas/EliminarFunsion';
+
 function Admi() {
   const [active, setActive] = useState('Cineplanet');
 
   return (
     <div>  
-     <FormPromociones /> 
+     <FormPromociones />
+     <FormFunsion/>
+     <EliminarFunsion/> 
      <CineForm />
     </div>
     );
