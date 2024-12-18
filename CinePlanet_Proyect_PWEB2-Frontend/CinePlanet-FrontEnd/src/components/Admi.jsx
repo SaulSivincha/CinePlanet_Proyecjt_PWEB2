8d8ac5c991eedac875f3cae8465f1bec02344b3f
roydanpe <rapazaan@unsa.edu.pe>
2024-12-17 22:12:11 -0500

    MOviendo el cineform

--- CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/Admi.jsx
+++ CinePlanet_Proyect_PWEB2-Frontend/CinePlanet-FrontEnd/src/components/Admi.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import '../styles/Admi.css';
 import FormPromociones from './FormPromociones';
+import CineForm from './CineForm';
 function Admi() {
   const [active, setActive] = useState('Cineplanet');
 
   return (
     <div>  
      <FormPromociones /> 
+     <CineForm />
     </div>
     );
 };
